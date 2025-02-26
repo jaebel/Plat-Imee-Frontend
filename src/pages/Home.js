d631b3a76edc27058dedf4f38476561d53adea81
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-02-26 02:50:24 +0000

    Replace default app.js with something more relevant

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Plat-Imee</h1>
+      <p>Your personal anime discovery and recommendation platform.</p>
+    </div>
+  );
+};
+
+export default Home;
